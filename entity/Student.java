51660a4f1897c96624067ce91ba717bb4e7f264b
Shametha K G <59509542+shametha@users.noreply.github.com>
2022-07-18 22:40:15 +0530

    Entity

---
+++ entity/Student.java
@@ -0,0 +1,42 @@
+package com.example.demoKafka.entity;
+
+public class Student {
+    int id;
+    String firstName;
+    String lastName;
+    String branch;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", branch='" + branch + '\'' +
+                '}';
+    }
+}
